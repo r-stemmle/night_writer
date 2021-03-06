92e357511b60bd1bcb0236d0c58c60617366c8b1
r-stemmle <ryan.stemmle@gmail.com>
2021-03-06 08:25:25 -0500

    Complete iteration 1

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,15 +1,47 @@
 class NightWriter
 
+  attr_reader :import_file,
+              :export_file
+
+  def initialize(import_file, export_file)
+    @import_file = import_file
+    @export_file = export_file
+  end
+
+  #tested
+  def import_file_path
+    "./data/" + import_file
+  end
+
+  #not tested
+  def export_file_path
+    "./braille/" + export_file
+  end
+
+  def make_braille
+    File.write(export_file_path, read_message)
+  end
+
+  def message_file
+    message = File.new(import_file_path)
+  end
+
+  def read_message
+    message_file.read.chomp
+  end
+
   def character_count
-    256
+    read_message.length
   end
 
   def confirmation_message
-    "Created 'braille.txt' containing #{character_count} characters"
+    "Created '#{export_file}' containing #{character_count} characters"
   end
-
 
 end
 
-ryan = NightWriter.new
-puts ryan.confirmation_message
+import_file = ARGF.argv.first
+export_file = ARGF.argv.last
+
+night_writer = NightWriter.new(import_file, export_file)
+# puts night_writer.confirmation_message
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -6,26 +6,55 @@ require 'mocha/minitest'
 class NightWriterTest < Minitest::Test
 
   def test_it_exists
-    night_writer = NightWriter.new
-    night_writer.stubs(:new).returns()
+    night_writer = NightWriter.new("message.txt", "braille.txt")
 
     assert_instance_of NightWriter, night_writer
   end
 
   def test_it_has_a_confirmation_message
-    night_writer = NightWriter.new
-    night_writer.stubs(:new).returns()
+    night_writer = NightWriter.new("message.txt", "braille.txt")
     expected = "Created 'braille.txt' containing 256 characters"
     night_writer.stubs(:character_count).returns(256)
+    night_writer.stubs(:export_file).returns("braille.txt")
 
     assert_equal expected, night_writer.confirmation_message
   end
 
-  def test_it_can_count_message_characters
-    night_writer = NightWriter.new
-    night_writer.stubs(:new).returns()
+  def test_it_can_create_export_file
+    night_writer = NightWriter.new("message.txt", "braille.txt")
 
-    assert_equal 256, night_writer.character_count
+    assert_equal "braille.txt", night_writer.export_file
+  end
+
+  def test_it_can_find_import_file_path
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+
+    assert_equal "./data/message.txt", night_writer.import_file_path
+  end
+
+  def test_it_can_find_export_file_path
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+
+    assert_equal "./braille/braille.txt", night_writer.export_file_path
+  end
+
+  def test_it_can_produce_message_text_as_string
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+
+    assert_equal String, night_writer.read_message.class
+  end
+
+  def test_it_can_read_message_character_length
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+    night_writer.stubs(:read_message).returns("Hello")
+
+    assert_equal 5, night_writer.character_count
+  end
+
+  def test_it_can_create_braille_output_text_file
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+
+    assert_equal Integer, night_writer.make_braille.class
   end
 
 end
